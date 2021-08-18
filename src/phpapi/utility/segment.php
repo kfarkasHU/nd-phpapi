fcb48f6aa45e873f0bff1d490ef08243b8bf7f5a
sohamar <sohamar@farkask.eu>
2021-08-18 19:57:48 +0200

    use calculated value resolve #2

--- src/phpapi/utility/segment.php
+++ src/phpapi/utility/segment.php
@@ -1,13 +1,13 @@
 <?php
 namespace PhpAPI2 {
     const SEGMENT_SEPARATOR = "/";
-    const OPTIONAL_PARAM_MARK = ":";
+    const PARAM_MARK = ":";
     class Segments {
         public static function ToNodes($uri) {
             return explode(SEGMENT_SEPARATOR, $uri);
         }
         public static function IsParameter($segment) {
-            return substr($segment, 0, 1) === OPTIONAL_PARAM_MARK;
+            return substr($segment, 0, strlen(PARAM_MARK)) === PARAM_MARK;
 
         }
         public static function NormalizeParameter($segment) {
